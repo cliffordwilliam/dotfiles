d1430bad4510874d0a306bb28b0f1c48942dd209
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-08 17:00:56 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,104 @@
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      transparent_background = true,
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").setup({})
+      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "williamboman/mason.nvim", opts = {} },
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("my-lsp", { clear = true }),
+        callback = function(event)
+          vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { buffer = event.buf })
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = event.buf })
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = event.buf })
+        end,
+      })
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pylsp", "ts_ls", "clangd" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({ capabilities = capabilities })
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function() end,
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
+})
+
