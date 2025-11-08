67b54f70cbe6325b0f3d9995df3ed3df936e9821
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-08 17:51:56 +0700

    Update install_nvim_setup.sh

--- install_nvim_setup.sh
+++ install_nvim_setup.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 sudo pacman -S --noconfirm ripgrep fd python-lsp-server clang unzip
 
+if command -v npm >/dev/null 2>&1; then
+  npm install -g typescript typescript-language-server
+else
+  echo "npm not found in this session, ts lsp deps are not installed!"
+fi
+
 mkdir -p ~/.config/nvim
 
 curl -fsSL https://raw.githubusercontent.com/cliffordwilliam/dotfiles/main/init.lua -o ~/.config/nvim/init.lua
