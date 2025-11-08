ce7ac041facdc0927722ec99120ef570c6ad82cc
Clifford William <ccliffordwilliam@gmail.com>
2025-11-08 20:25:29 +0700

    feat: add feh

---
+++ feh_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+IMG_NAME="b-422.jpg"
+IMG_URL="https://raw.githubusercontent.com/cliffordwilliam/dotfiles/main/$IMG_NAME"
+IMG_DIR="$HOME/images"
+IMG_PATH="$IMG_DIR/$IMG_NAME"
+XINITRC="$HOME/.xinitrc"
+FEH_CMD="feh --bg-fill \"$IMG_PATH\""
+
+sudo pacman -Sy --noconfirm feh
+
+mkdir -p "$IMG_DIR"
+
+curl -L "$IMG_URL" -o "$IMG_PATH"
+
+touch "$XINITRC"
+
+sed -i '/feh --bg-fill/d' "$XINITRC"
+
+sed -i "1i $FEH_CMD" "$XINITRC"
+
+echo "Done! Your background will be set when you start X."
+
