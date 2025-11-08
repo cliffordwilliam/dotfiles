b90cf32939a6af77331b7fbbd1963e47cf31adb3
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-08 17:48:35 +0700

    Create setup_node.sh

---
+++ setup_node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+nvm use --lts
