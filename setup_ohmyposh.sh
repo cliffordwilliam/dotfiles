c382c59d1060488364050fc1db414d29c398b77e
Clifford William <ccliffordwilliam@gmail.com>
2025-11-09 16:44:39 +0700

    feat: add ohmypost setup

---
+++ setup_ohmyposh.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+HOME_DIR="$HOME"
+CONFIG_DIR="$HOME_DIR/.config/oh-my-posh"
+CONFIG_FILE="$CONFIG_DIR/catppuccin_mocha.omp.json"
+BASHRC="$HOME_DIR/.bashrc"
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+export PATH=$PATH:$HOME/.local/bin
+
+eval "$(oh-my-posh init bash)"
+
+oh-my-posh font install Meslo
+
+mkdir -p "$CONFIG_DIR"
+
+curl -fsSL https://raw.githubusercontent.com/cliffordwilliam/dotfiles/main/catppuccin_mocha.omp.json -o "$CONFIG_FILE"
+
+EVAL_LINE="eval \"\$(oh-my-posh init bash --config $CONFIG_FILE)\""
+if ! grep -Fxq "$EVAL_LINE" "$BASHRC"; then
+    echo "$EVAL_LINE" >> "$BASHRC"
+fi
+
+echo "Success! Reloading Bash..."
+exec bash
+
