190b25d6b5d6b9fe44f9ef69e89e8b6429304a48
Clifford William <ccliffordwilliam@gmail.com>
2025-11-09 17:08:07 +0700

    feat: add tmux

---
+++ setup_tmux.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+PLUGIN_DIR="$HOME/.config/tmux/plugins/catppuccin/tmux"
+CONFIG_DIR="$HOME/.config/tmux"
+TMUX_CONF="$CONFIG_DIR/tmux.conf"
+REPO_URL="https://github.com/catppuccin/tmux.git"
+BRANCH="main"
+RUN_LINE="run \"$PLUGIN_DIR/catppuccin.tmux\""
+
+mkdir -p "$(dirname "$PLUGIN_DIR")"
+
+if [ -d "$PLUGIN_DIR/.git" ]; then
+    git -C "$PLUGIN_DIR" fetch --all
+    git -C "$PLUGIN_DIR" checkout "$BRANCH"
+    git -C "$PLUGIN_DIR" pull origin "$BRANCH"
+else
+    git clone -b "$BRANCH" "$REPO_URL" "$PLUGIN_DIR"
+fi
+
+mkdir -p "$CONFIG_DIR"
+
+if [ ! -f "$TMUX_CONF" ]; then
+    touch "$TMUX_CONF"
+fi
+
+if ! grep -Fxq "$RUN_LINE" "$TMUX_CONF"; then
+    echo "$RUN_LINE" >> "$TMUX_CONF"
+fi
+
+echo "Done!"
+
