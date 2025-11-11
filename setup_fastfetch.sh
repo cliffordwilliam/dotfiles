2dfb9f07f3a2d1def7327e99ff42507fe90218f7
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-11 07:01:12 +0700

    Update setup_fastfetch.sh

--- setup_fastfetch.sh
+++ setup_fastfetch.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 set euo pipefail
 
-IMG_NAME="logo.png"
-IMG_URL="https://raw.githubusercontent.com/cliffordwilliam/dotfiles/main/$IMG_NAME"
-IMG_DIR="$HOME/images"
-IMG_PATH="$IMG_DIR/$IMG_NAME"
-BASHRC="$HOME/.bashrc"
-
 sudo pacman -Sy --noconfirm fastfetch
 
-mkdir -p "$IMG_DIR"
-
-curl -L "$IMG_URL" -o "$IMG_PATH"
-
-FASTFETCH_LINE="fastfetch --logo \"$IMG_PATH\" --logo-type kitty-direct --logo-padding-left 1 --logo-padding-right 1 --logo-padding-top 1"
-
-grep -qxF "$FASTFETCH_LINE" "$BASHRC" || echo "$FASTFETCH_LINE" >> "$BASHRC"
+grep -qxF "fastfetch" "$HOME/.bashrc" || echo "fastfetch" >> "$HOME/.bashrc"
 
 echo "Done"
