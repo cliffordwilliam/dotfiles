22f082335a8e5947cd5fe894f29f34bfed4aa98f
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-11 17:28:39 +0700

    Update setup_i3_no_title_bar.sh

--- setup_i3_no_title_bar.sh
+++ setup_i3_no_title_bar.sh
@@ -9,7 +9,7 @@ touch "$I3CONF"
 sed -i '/^default_border/d' "$I3CONF"
 sed -i '/^default_floating_border/d' "$I3CONF"
 
-sed -i '1idefault_floating_border pixel 1' "$I3CONF"
-sed -i '1idefault_border pixel 1' "$I3CONF"
+sed -i '1idefault_floating_border pixel 0' "$I3CONF"
+sed -i '1idefault_border pixel 0' "$I3CONF"
 
 echo "Done"
