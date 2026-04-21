f246e658d16b21ab4ee1e1c9ac0749472d5ce34c
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2026-04-21 19:31:36 +0700

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman -Sy --needed --noconfirm docker docker-compose
+sudo pacman -Syu docker docker-compose
 sudo systemctl enable --now docker.service
-
 sudo usermod -aG docker "$USER"
 
 echo "Done"
