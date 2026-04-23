aab4cf73e3ef07af40bd4f0364e8a65e643a3ce0
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2026-04-23 21:54:15 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
+vim.o.signcolumn = "yes"
 vim.g.mapleader = " "
+vim.o.ignorecase = false
+vim.o.smartcase = false
+vim.opt.wrap = false
 vim.o.number = true
+vim.o.cursorline = true
 vim.o.clipboard = "unnamedplus"
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Grab lazy dependency manager idempotently.
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local repo = "https://github.com/folke/lazy.nvim.git"
@@ -13,18 +19,100 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Declare what dependencies I need here.
 require("lazy").setup(
     {
+        -- Grab theme.
+        {
+            "folke/tokyonight.nvim",
+            priority = 1000,
+            config = function()
+                vim.cmd.colorscheme "tokyonight-night"
+            end
+        },
+        -- Grab file finder.
         {
             "nvim-telescope/telescope.nvim",
             dependencies = {"nvim-lua/plenary.nvim"},
             config = function()
-                vim.keymap.set("n", "<leader>ff", function()
-                    require("telescope.builtin").find_files(
-                            { hidden = true, file_ignore_patterns = { "^%.git/" } }
-                    )
-                end)
+                vim.keymap.set(
+                    "n",
+                    "<leader>ff",
+                    function()
+                        require("telescope.builtin").find_files({hidden = true, file_ignore_patterns = {"^%.git/"}})
+                    end
+                )
             end
+        },
+        -- Grab LSP manager.
+        {
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                "williamboman/mason.nvim",
+                "williamboman/mason-lspconfig.nvim"
+            },
+            -- Define LSP command list.
+            config = function()
+                vim.api.nvim_create_autocmd(
+                    "LspAttach",
+                    {
+                        group = vim.api.nvim_create_augroup("lsp-attach", {clear = true}),
+                        callback = function(event)
+                            -- gd 		    = go to definition.
+                            vim.keymap.set(
+                                "n",
+                                "gd",
+                                require("telescope.builtin").lsp_definitions,
+                                {buffer = event.buf}
+                            )
+                            -- gr 		    = go to references.
+                            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {buffer = event.buf})
+                            -- space rn 	= rename.
+                            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {buffer = event.buf})
+                            -- space e	 	= open error.
+                            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {buffer = event.buf})
+                            -- K 		    = show documentation.
+                            vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = event.buf})
+                        end
+                    }
+                )
+                -- My LSP collections. Some of these needs external dependencies, like node runtime and so on.
+                local servers = {
+                    lua_ls = {},
+                    ts_ls = {}
+                }
+
+                require("mason").setup()
+
+                require("mason-lspconfig").setup(
+                    {
+                        ensure_installed = vim.tbl_keys(servers),
+                        handlers = {
+                            function(server_name)
+                                local opts = servers[server_name] or {}
+                                require("lspconfig")[server_name].setup(opts)
+                            end
+                        }
+                    }
+                )
+            end
+        },
+        -- Setup autocompletion.
+        {
+            "saghen/blink.cmp",
+            version = "v0.*", -- Use a stable release.
+            opts = {
+                -- Autocompletion command list.
+                keymap = {
+                    ["<Up>"] = {"select_prev", "fallback"},
+                    ["<Down>"] = {"select_next", "fallback"},
+                    ["<Enter>"] = {"accept", "fallback"}
+                },
+                -- Options sources are from file paths and LSP codebase knowledge.
+                sources = {
+                    default = {"lsp", "path"}
+                }
+            }
         }
     }
 )
